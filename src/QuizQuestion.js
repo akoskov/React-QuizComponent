02e0cc49bc11374dfd6aa1ca65065a120dbbe615
Ákos Kovács <akos.t.kovacs@ericsson.com>
2019-02-13 17:04:06 +0100

    Add QuizQuestions Component

---
+++ src/QuizQuestion.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class QuizQuestins extends Component {
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_questions.instruction_text}</p>
+                </section>
+                <section>
+                    <ul>
+                        <li>{this.props.quiz_questions.answer_options[0]}</li>
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestins
